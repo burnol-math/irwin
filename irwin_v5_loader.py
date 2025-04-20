e3c1733a34e870a889d02e8eb11a3a342fe12157
burnol_math <27213463-burnol_math@users.noreply.gitlab.com>
2025-04-20 12:57:11 +0200

    irwin_v5

---
+++ irwin_v5_loader.py
@@ -0,0 +1,23 @@
+# irwin_v5_loader.py
+# ceci permet de faire depuis l'invite de sage, par exemple:
+
+# from irwin_v5_loader import irwin as irwin2025, irwinpos as irwinpos2025
+
+# Ceci
+# afin de comparer avec le comportement
+# du irwin.sage de 2024 qui définit des fonctions
+# du même nom.
+
+# Cette méthode a de très gros problèmes potentiels
+# je pense si des fonctions auxiliaires de mêmes noms
+# mais différentes sont dans les fichiers.
+# Le souci est que je n'ai jamais pu accéder à
+# sage.misc.preparser.  Mais je pense que pour
+# comparer irwin_v3.sage (2025) et irwin.sage (2024)
+# ça va marcher.
+
+from sage.all import *
+
+load("irwin_v5.sage")
+irwin = globals()['irwin']
+irwinpos = globals()['irwinpos']
