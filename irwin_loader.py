b7f296d90532f6c1238654a1b1e142d60ec0ae83
burnol_math <27213463-burnol_math@users.noreply.gitlab.com>
2025-05-18 11:35:06 +0200

    modified:   irwin_loader.py

--- irwin_loader.py
+++ irwin_loader.py
@@ -1,12 +1,11 @@
 # irwin_loader.py
 # ceci permet de faire depuis l'invite de sage, par exemple:
 
-# from irwin_loader import irwin as irwin2024, irwinpos as irwinpos2024
+# from irwin_loader import irwin as irwin5, irwinpos as irwinpos5
 
-# Ceci
-# afin de comparer avec le comportement
-# du irwin.sage de 2024 qui définit des fonctions
-# du même nom.
+# Ceci par exemple
+# afin de comparer avec le comportement des procédures
+# du irwin_legacy.sage de 2024 ou de irwin_v3.sage de 2025.
 
 # Cette méthode a de très gros problèmes potentiels
 # je pense si des fonctions auxiliaires de mêmes noms
