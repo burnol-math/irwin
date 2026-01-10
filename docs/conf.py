cff638305bf671642dd9dca67400470f8a089d0d
burnol_math <27213463-burnol_math@users.noreply.gitlab.com>
2026-01-10 18:04:00 +0100

    [DOC] Update Copyright years in gitlab front page

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Irwin'
-copyright = '2025, Jean-François Burnol'
+copyright = '2025-2026, Jean-François Burnol'
 author = 'Jean-François Burnol'
 release = ''
 
